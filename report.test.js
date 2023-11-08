a952715daa9db3e3bae6db457d7456dfbe935fbf
diophants <diophants@gmail.com>
2023-11-08 08:24:22 +0300

    crawl app

--- report.test.js
+++ report.test.js
@@ -1,34 +1,15 @@
-const { sortPages } = require('./report');
-const { test, expect } = require('@jest/globals');
+// const { sortPages } = require('./report');
+// const { test, expect } = require('@jest/globals');
 
-test('sortPages 2 pages', () => {
-  const input = {
-    'https://wagslane.dev/path': 1,
-    'https://wagslane.dev': 3,
-  };
-  const actual = sortPages(input);
-  const expected = [
-    ['https://wagslane.dev', 3],
-    ['https://wagslane.dev/path', 1],
-  ];
-  expect(actual).toEqual(expected);
-});
-
-test('sortPages 5 pages', () => {
-  const input = {
-    'https://wagslane.dev/path': 1,
-    'https://wagslane.dev': 3,
-    'https://wagslane.dev/path1': 42,
-    'https://wagslane.dev/path2': 23,
-    'https://wagslane.dev/path3': 7,
-  };
-  const actual = sortPages(input);
-  const expected = [
-    ['https://wagslane.dev/path1', 42],
-    ['https://wagslane.dev/path2', 23],
-    ['https://wagslane.dev/path3', 7],
-    ['https://wagslane.dev', 3],
-    ['https://wagslane.dev/path', 1],
-  ];
-  expect(actual).toEqual(expected);
-});
+// test('sortPages', () => {
+//   const input = {
+//     'https://wagslane.dev/path': 1,
+//     'https://wagslane.dev': 3,
+//   };
+//   const actual = sortPages(input);
+//   const expect = [
+//     ['https://wagslane.dev', 3],
+//     ['https://wagslane.dev/path', 1],
+//   ];
+//   expect(actual).toEqual(expect);
+// });
